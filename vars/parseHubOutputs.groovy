6978b3c583ce2b32fbf4858d62460ea39577eddc
Nikolay Arhipov <nikolay@agilestacks.com>
2018-05-14 15:04:40 +0300

    Debug log

--- vars/parseHubOutputs.groovy
+++ vars/parseHubOutputs.groovy
@@ -3,6 +3,10 @@
 def call(def log) {
     def paramFormat = /\d{4}(?:\/\d{2}){2} \d{2}(?::\d{2}){2}\s+(?:\[.*\]){0,1}\s([\d\w-_\.]+:[\d\w-_\.]+) => `(.*)`/
 
+    println("------------------")
+    println(log)
+    println('------------------')
+
     return log
             .tokenize('\n')
             .collect { it =~ paramFormat }
